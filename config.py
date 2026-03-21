b06b823ed9225fef586ee05469923a71ac553a82
artiommuntean8-ui <artiommuntean8@gmail.com>
2026-03-21 21:44:27 +0200

    Refactored security

--- config.py
+++ config.py
@@ -1,7 +1,8 @@
 # config.py
+import os
 
 # Înlocuiește cu token-ul primit de la @BotFather
-BOT_TOKEN = "8212371554:AAEyXjXL-aCCBlCe0mZMeI_FXOLP0vhd4uA"
+BOT_TOKEN = os.getenv("BOT_TOKEN")
 
 # Numele fișierului de bază de date
 DB_NAME = "trading_bot.db"
